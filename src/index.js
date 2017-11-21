c0677d5403d847682d6b0651dd59eaddf66c3208
Rekha Mundada <rekhamundada@gmail.com>
2017-11-21 11:29:41 -0500

    got maps

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
 console.log('I AM WORKING!')
+
+const mapboxgl = require("mapbox-gl");
+
+mapboxgl.accessToken = 'pk.eyJ1IjoicmVraGFtdW5kYWRhIiwiYSI6ImNqYTl0emI5djBscnczM2xpbXgzOXMyeDAifQ.7BU2fUbgmkep7gGabrHU6Q';
+
+const map = new mapboxgl.Map({
+  container: "map",
+  center: [-74.009, 40.705], // FullStack NY coordinates; alternatively, use [-87.6354, 41.8885] for Chicago
+  zoom: 12, // starting zoom
+  style: "mapbox://styles/mapbox/streets-v10" // mapbox has lots of different map styles available.
+});
